8447cdf9f58f757bee42abfb8a73cf97c0c88780
Marta Chaika <janeseagul@gmail.com>
2023-02-24 15:31:54 +0300

    Удалила ненужные скрипты

--- bot_send_text_message.py
+++
@@ -1,8 +0,0 @@
-import os
-import telegram
-token = os.environ['BOT_API']
-chat_id = os.environ['CHAT_ID']
-
-bot = telegram.Bot(token=token)
-bot.send_message(chat_id = chat_id, text = 'Привет! Хочешь получить фотографии с запуска SpaceX?')
-
