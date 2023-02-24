30052482e58245a3d90130fea3f39f2196972ce7
Marta Chaika <janeseagul@gmail.com>
2023-02-24 15:29:48 +0300

    Скрипт для загрузки фото и расширения файла

--- load_main.py
+++ load_main.py
@@ -3,11 +3,13 @@ from urllib.parse import urlparse
 
 import requests
 
+
 def load_picture(url, filename, params=None):
 	response = requests.get(url, params=params)
 	response.raise_for_status()
 	with open(filename, "wb") as file:
 		file.write(response.content)
+
 
 def get_file_ext(url):
 	path = urlparse(url).path
