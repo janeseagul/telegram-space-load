a37a6a78a19a2cb6bd1e133ccb4ad1a728b5f34d
Marta Chaika <janeseagul@gmail.com>
2023-02-25 15:19:11 +0300

    Исправила код

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -2,27 +2,24 @@ import os
 import requests
 import argparse
 
-from urllib.parse import urlparse
-from os.path import splitext
-
 from load_main import get_file_ext, load_picture
 
 
-def fetch_nasa_apod(count, nasa_api):
-    params = {"api_key": nasa_api, "count": count}
+def fetch_nasa_apod(count, nasa_api_key):
+    params = {"api_key": nasa_api_key, "count": count}
     response = requests.get("https://api.nasa.gov/planetary/apod", params=params)
     response.raise_for_status()
     for number, apod in enumerate(response.json()):
         if apod["media_type"] == "image":
             url = apod["url"]
             ext = get_file_ext(url)
-            filename = os.path.join("Images", f"nasa_apod_{number}{ext}")
-            load_picture(url, filename)
+            filepath = os.path.join("Images", f"nasa_apod_{number}{ext}")
+            load_picture(url, filepath)
 
 
 if __name__ == "__main__":
     os.makedirs("Images", exist_ok=True)
-    nasa_api = os.environ['NASA_API']
+    nasa_api_key = os.environ['NASA_API']
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-c",
@@ -31,4 +28,4 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    fetch_nasa_apod(args.count, nasa_api)
+    fetch_nasa_apod(args.count, nasa_api_key)
